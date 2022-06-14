69561eae9f6ed3bc02949265d2b04a7950abc17a
G4d2hick <gadzhimurad.2016.magomedov@mail.ru>
2022-06-14 23:23:07 +0300

    Many components have been added

---
+++ src/components/skeleton/Skeleton.js
@@ -0,0 +1,21 @@
+import './skeleton.scss';
+
+const Skeleton = () => {
+
+    return (
+        <>
+            <p className="char__select">Пожалуйста, выберите персонажа, чтобы просмотреть информацию</p>
+            <div className="skeleton">
+                <div className="pulse skeleton__header">
+                    <div className="pulse skeleton__circle"></div>
+                    <div className="pulse skeleton__mini"></div>
+                </div>
+                <div className="pulse skeleton__block"></div>
+                <div className="pulse skeleton__block"></div>
+                <div className="pulse skeleton__block"></div>
+            </div>
+        </>
+    )
+}
+
+export default Skeleton;
---
+++ src/services/MarvelServise.js
@@ -0,0 +1,36 @@
+class MarvelServise {
+
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=d8c777e998942e7f7bc4ed3169f3ea17'
+
+    getResourse = async (url) => {
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Не удалось получить ответ от сервера ${url}, статус: ${res.status}`)
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacters)
+    }
+    getCharacters = async (id) => {
+        const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacters(res.data.results[0])
+    }
+    _transformCharacters = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ? char.description : 'Описание отсутствует', thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+            comics: char.comics.items
+        }
+    }
+}
+
+export default MarvelServise;
